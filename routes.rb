94523970e3f60ebb1c069f778c2c6469d2427608
Skylar MacDonald <skylar@skylarmacdonald.com>
2018-03-08 20:20:47 -0500

    Parse request body JSON into hash

--- routes.rb
+++ routes.rb
@@ -10,27 +10,30 @@ class YNABHook < Sinatra::Base
   header = {'Content-Type': 'text/json', 'Authorization': "Bearer #{ENV['YNAB_KEY']}"}
 
   post '/monzo' do
-    if params[:type] == 'transaction.created'
+    payload = JSON.parse(request.body.read)
+    if payload['type'] == 'transaction.created'
       uri = URI.parse("https://api.youneedabudget.com/v1/budgets/#{ENV['BUDGET_ID']}/transactions")
       http = Net::HTTP.new(uri.host, uri.port)
       txn = {
         "transaction": {
           "account_id": ENV['MONZO_ACCOUNT_ID'],
-          "date": DateTime.iso8601(params[:data][:created]).strftime('%F'),
-          "amount": params[:data][:amount]*10,
+          "date": DateTime.iso8601(payload['data']['created']).strftime('%F'),
+          "amount": payload['data']['amount']*10,
           "payee_id": nil,
-          "payee_name": params[:data][:description],
+          "payee_name": payload['data']['description'],
           "category_id": nil,
           "memo": nil,
           "cleared": "cleared",
           "approved": true,
           "flag_color": nil,
-          "import_id": params[:data][:id]
+          "import_id": payload['data']['id']
         }
       }
       request = Net::HTTP::Post.new(uri.request_uri, header)
       request.body = txn.to_json
       response = http.request(request)
+    else
+      'OK'
     end
   end
 
